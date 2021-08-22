e20288622daa239790bd8220cea100d5ab34d0fd
Sadid <ulamuyaman@gmail.com>
2021-08-22 21:33:05 +0300

    issue with text_screen

--- lib/data/book_map.dart
+++ lib/data/book_map.dart
@@ -10,3 +10,13 @@ class BookMap {
     return body.map((e) => Book.fromJson(e)).toList();
   }
 }
+
+class TextsMap {
+  static Future<List<Texts>> getTextsLocally(BuildContext context) async {
+    final assetBundle = DefaultAssetBundle.of(context);
+    final data = await assetBundle.loadString('lib/data/book.json');
+    final List<dynamic> body = json.decode(data);
+    return body.map((e) => Texts.fromJson(e)).toList();
+  }
+}
+
--- lib/screens/text_screen.dart
+++ lib/screens/text_screen.dart
@@ -4,18 +4,20 @@ import 'package:avrod/data/book_map.dart';
 import 'package:flutter/material.dart';
 
 class TextScreen extends StatefulWidget {
-  final Book? books;
-  final int? textIndex;
-  const TextScreen({Key? key, this.books, this.textIndex}) : super(key: key);
+ final int textIndex;
+  const TextScreen(Key? key, this.textIndex) : super(key: key);
   @override
   _TextScreenState createState() => _TextScreenState();
 }
 
 class _TextScreenState extends State<TextScreen> {
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: const Text('Лафзи бобҳо'),
+        centerTitle: true,
+      ),
       body: Container(
         decoration: const BoxDecoration(
             gradient: LinearGradient(
@@ -23,13 +25,13 @@ class _TextScreenState extends State<TextScreen> {
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
                 stops: [0.3, 0.7])),
-        child: FutureBuilder<List<Book>>(
-          future: BookMap.getBookLocally(context),
+        child: FutureBuilder<List<Texts>>(
+          future: TextsMap.getTextsLocally(context),
           builder: (contex, snapshot) {
-            final books = snapshot.data;
+            final texts = snapshot.data;
 
             if (snapshot.hasData) {
-              return buildBook(books![widget.textIndex ?? 0]);
+              return buildBook(texts![widget.textIndex]);
             } else if (snapshot.hasError) {
               return const Center(
                 child: Text('Some erro occured'),
@@ -40,20 +42,16 @@ class _TextScreenState extends State<TextScreen> {
           },
         ),
       ),
-      
     );
-    
   }
 
-  Widget buildBook(Book book) {
-    
+  Widget buildBook(Texts texts) {
     return ListView.builder(
-      
-        itemCount: book.chapters!.length,
+        itemCount: texts.text!.length,
         itemBuilder: (context, index) {
-          final text = book.chapters![index].texts;
+          final text = texts.text![index];
           // ignore: avoid_unnecessary_containers
-          return Container(child: Center(child: Text(text?[index].id ?? '')));
+          return Container(child: Center(child: Text(text)));
         });
   }
 }
