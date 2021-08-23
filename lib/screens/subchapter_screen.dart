aff777577574816476074350d99f8c788b9f05a0
Sadid <ulamuyaman@gmail.com>
2021-08-23 14:33:15 +0300

    issue with displaying text_screen

--- lib/screens/subchapter_screen.dart
+++ lib/screens/subchapter_screen.dart
@@ -3,10 +3,10 @@ import 'package:avrod/colors/colors.dart';
 import 'package:avrod/data/book_class.dart';
 import 'package:avrod/data/book_map.dart';
 import 'package:avrod/screens/text_screen.dart';
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:like_button/like_button.dart';
-
 
 class SubchapterScreen extends StatefulWidget {
   const SubchapterScreen(this.bookIndex, {Key? key}) : super(key: key);
@@ -17,21 +17,28 @@ class SubchapterScreen extends StatefulWidget {
 }
 
 class _SubchapterScreenState extends State<SubchapterScreen> {
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-          elevation: 0.0,
-          backgroundColor: gradientStartColor,
-          title: const Text('Рӯйхати бобҳо'),
-          centerTitle: true),
+        elevation: 0.0,
+        backgroundColor: gradientStartColor,
+        title: const Text('Рӯйхати бобҳо'),
+        centerTitle: true,
+        flexibleSpace: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [gradientStartColor, gradientEndColor],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 0.7])),
+        ),
+      ),
       // ignore: avoid_unnecessary_containers
       body: Container(
         decoration: const BoxDecoration(
             gradient: LinearGradient(
-                colors: [ gradientStartColor, gradientEndColor],
+                colors: [gradientStartColor, gradientEndColor],
                 begin: Alignment.topCenter,
                 end: Alignment.bottomCenter,
                 stops: [0.3, 0.7])),
@@ -67,75 +74,80 @@ class _SubchapterScreenState extends State<SubchapterScreen> {
         itemCount: books.chapters?.length ?? 0,
         itemBuilder: (context, index) {
           final chapters = books.chapters![index];
-         
 
           // ignore: sized_box_for_whitespace
           return Padding(
             padding: const EdgeInsets.all(5.0),
             child: InkWell(
               onTap: () {
-              
-               Navigator.push(context, MaterialPageRoute(builder: (context) {
-                 return const TextScreen();
+                Navigator.push(context, MaterialPageRoute(builder: (context) {
+                  return const TextScreen();
                 }));
               },
-              child: Container(
-                decoration: BoxDecoration(
-                    image: DecorationImage(
-                        image: NetworkImage(chapters.listimage!),
-                        fit: BoxFit.cover),
-                    boxShadow: const [
-                      BoxShadow(
-                          color: Colors.black38,
-                          offset: Offset(0.0, 2.0),
-                          blurRadius: 6.0)
+              child: CachedNetworkImage(
+                imageUrl:  chapters.listimage!,
+                imageBuilder: (context, imageProvider) {
+                return Container(
+                  decoration: BoxDecoration(
+                      image: DecorationImage(
+                          image:imageProvider,
+                          fit: BoxFit.cover),
+                      boxShadow: const [
+                        BoxShadow(
+                            color: Colors.black38,
+                            offset: Offset(0.0, 2.0),
+                            blurRadius: 6.0)
+                      ],
+                      gradient: const LinearGradient(
+                          colors: [Colors.white54, secondaryTextColor],
+                          begin: Alignment.centerLeft,
+                          end: Alignment.centerRight),
+                      borderRadius:
+                          const BorderRadius.all(Radius.circular(16.0))),
+                  child: Stack(
+                    children: [
+                      Center(
+                        child: Container(
+                          decoration: const BoxDecoration(
+                              color: Colors.black26,
+                              borderRadius:
+                                  BorderRadius.all(Radius.circular(16.0))),
+                          height: 250,
+                          width: 170,
+                        ),
+                      ),
+                      ListTile(
+                        title: Center(
+                          child: Text(
+                            chapters.name!,
+                            textAlign: TextAlign.center,
+                            style: const TextStyle(
+                                fontSize: 22.0,
+                                fontWeight: FontWeight.w600,
+                                color: titleTextColor),
+                          ),
+                        ),
+                      ),
+                      const Positioned(
+                        top: 15,
+                        right: 10,
+                        child: LikeButton(
+                          
+                          size: 40,
+                          circleColor: CircleColor(
+                              start: Color(0xffDC143C), end: Color(0xffFF0000)),
+                          bubblesColor: BubblesColor(
+                            dotPrimaryColor: Color(0xffFF0000),
+                            dotSecondaryColor: Color(0xffFF0000),
+                          ),
+
+                          
+                        ),
+                      )
                     ],
-                    gradient: const LinearGradient(
-                        colors: [Colors.white54, secondaryTextColor],
-                        begin: Alignment.centerLeft,
-                        end: Alignment.centerRight),
-                    borderRadius:
-                        const BorderRadius.all(Radius.circular(16.0))),
-                child: Stack(
-                  children: [
-                    Center(
-                      child: Container(
-                        decoration: const BoxDecoration(
-                            color: Colors.black26,
-                            borderRadius:
-                                BorderRadius.all(Radius.circular(16.0))),
-                        height: 250,
-                        width: 170,
-                      ),
-                    ),
-                    ListTile(
-                      title: Center(
-                        child: Text(
-                          chapters.name!,
-                          textAlign: TextAlign.center,
-                          style: const TextStyle(
-                              fontSize: 22.0,
-                              fontWeight: FontWeight.w600,
-                              color: titleTextColor),
-                        ),
-                      ),
-                    ),
-                    const Positioned(
-                      top: 15,
-                      right: 10,
-                      child: LikeButton(
-                        size: 40,
-                        circleColor: CircleColor(
-                            start: Color(0xffDC143C), end: Color(0xffFF0000)),
-                        bubblesColor: BubblesColor(
-                          dotPrimaryColor: Color(0xffFF0000),
-                          dotSecondaryColor: Color(0xffFF0000),
-                        ),
-                      ),
-                    )
-                  ],
-                ),
-              ),
+                  ),
+                );
+              }),
             ),
           );
         });
--- lib/screens/text_screen.dart
+++ lib/screens/text_screen.dart
@@ -14,33 +14,52 @@ class TextScreen extends StatefulWidget {
 class _TextScreenState extends State<TextScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Лафзи бобҳо'),
-        centerTitle: true,
-      ),
-      body: Container(
-        decoration: const BoxDecoration(
-            gradient: LinearGradient(
-                colors: [gradientStartColor, gradientEndColor],
-                begin: Alignment.topCenter,
-                end: Alignment.bottomCenter,
-                stops: [0.3, 0.7])),
-        child: FutureBuilder<List<Texts>>(
-          future: TextsMap.getTextsLocally(context),
-          builder: (contex, snapshot) {
-            final texts = snapshot.data;
-
-            if (snapshot.hasData) {
-              return buildBook(texts![widget.textIndex ?? 0]);
-            } else if (snapshot.hasError) {
-              return const Center(
-                child: Text('Some erro occured'),
-              );
-            } else {
-              return const CircularProgressIndicator();
-            }
-          },
+    return DefaultTabController(
+      length: 4,
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0.0,
+          title: const Text('Лафзи дуо'),
+          centerTitle: true,
+          flexibleSpace: Container(
+            decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [gradientStartColor, gradientEndColor],
+                  begin: Alignment.topLeft,
+                  end: Alignment.bottomRight,
+                  stops: [0.3, 0.7])),
+          ),
+          bottom: const TabBar(tabs: [
+           Tab(text: 'Дуои 1',),
+           Tab(text: 'Дуои 2',),
+           Tab(text: 'Дуои 3',),
+           Tab(text: 'Дуои 4',),
+          ],),
+           
+        ),
+        body: Container(
+          decoration: const BoxDecoration(
+              gradient: LinearGradient(
+                  colors: [gradientStartColor, gradientEndColor],
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  stops: [0.3, 0.7])),
+          child: FutureBuilder<List<Texts>>(
+            future: TextsMap.getTextsLocally(context),
+            builder: (contex, snapshot) {
+              final texts = snapshot.data;
+    
+              if (snapshot.hasData) {
+                return buildBook(texts![widget.textIndex ?? 0] );
+              } else if (snapshot.hasError) {
+                return const Center(
+                  child: Text('Some erro occured'),
+                );
+              } else {
+                return const CircularProgressIndicator();
+              }
+            },
+          ),
         ),
       ),
     );
@@ -50,9 +69,15 @@ class _TextScreenState extends State<TextScreen> {
     return ListView.builder(
         itemCount: texts.text?.length ?? 0,
         itemBuilder: (context, index) {
-          //final text = texts.text![index];
+          final id = texts.id![index];
           // ignore: avoid_unnecessary_containers
-          return Container(child: Center(child: Text(texts.id ?? '', style: const TextStyle(fontSize: 25.0),)));
+          return TabBarView(children:[
+            // ignore: avoid_unnecessary_containers
+            Container(
+             
+              child: 
+            Center(child: Text(id, style: const TextStyle(fontSize: 25.0),)))
+          ] );
         });
   }
 }
